d50a4e39ac401951d214249181e243990414b475
Bhavesh Kuamawat <bhavesh461@outlook.com>
2026-07-21 02:14:05 +0530

    Add input feature and few functions

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ for(let i = 0 ; i<3 ; i++){
 }
 
 console.log(gameboard);
+
+function players(){
+    const player1 = "A";
+    const player2 = "B";
+}
+
+function score(){
+    let score = 0;
+}
+
+function inputs(){
+    let input1 = "X";
+    let input2 = "O";
+}
+
+function playGame(row,column,input){
+    function process(){
+        gameboard[row][column] = input;
+    }
+    process();
+}
+playGame(1,1,"X");
+console.log(gameboard);
