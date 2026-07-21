dde7c86eea42026a88a1d274eba89919db5ad30f
Bhavesh Kuamawat <bhavesh461@outlook.com>
2026-07-22 02:43:46 +0530

    Complete console version
    I have completed a basic structure and removed un-
    necessary functions, also i have made a function
    which checks the logic to win

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 const gameboard = [];
 
-for(let i = 0 ; i<3 ; i++){
+for (let i = 0; i < 3; i++) {
     gameboard[i] = [];
-    for(let j = 0 ;j<3 ;j++){
-        gameboard[i].push(0);
+    for (let j = 0; j < 3; j++) {
+        gameboard[i].push(" ");
     }
 }
 
 console.log(gameboard);
 
-function players(){
-    const player1 = "A";
-    const player2 = "B";
-}
-
-function score(){
-    let score = 0;
-}
-
-function inputs(){
-    let input1 = "X";
-    let input2 = "O";
-}
-
-function playGame(row,column,input){
-    function process(){
+function playGame(row, column, input) {
+    function process() {
         gameboard[row][column] = input;
     }
     process();
 }
+
+function result() {
+    if ((gameboard[0][0] === gameboard[1][1] && gameboard[1][1] ===  gameboard[2][2]) || (gameboard[0][2] === gameboard[1][1] && gameboard[1][1]=== gameboard[2][0]) || (gameboard[1][0] === gameboard[1][1] && gameboard[1][1] === gameboard[1][2]) || (gameboard[0][1] === gameboard[1][1] && gameboard[1][1] === gameboard[2][1])) {
+
+        console.log("You won");
+    }
+
+    else{
+        console.log("tie");
+    }
+}
+playGame(0,0,"X");
 playGame(1,1,"X");
+playGame(2,2,"X");
 console.log(gameboard);
+result();
